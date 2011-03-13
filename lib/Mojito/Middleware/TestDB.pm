27153815ab25e1a5905d01b195c4db439c21e66f
Mateu X Hunter <hunter@missoula.org>
2011-03-13 13:23:16 -0600

    Add middleware to set db_name during RELEASE_TESTING

---
+++ lib/Mojito/Middleware/TestDB.pm
@@ -0,0 +1,21 @@
+use strictures 1;
+package Mojito::Middleware::TestDB;
+use parent qw(Plack::Middleware);
+
+# This middleware needs to be wrapped in after Mojito::Middleware 
+# which provides the $env->{mojito} object for which we adjust
+# the connecting DB to a test one.
+
+sub call {
+    my ( $self, $env ) = @_;
+    # This should be available since we wrap Mojito::Middleware around it
+    # See the builder block.
+    if ($env->{mojito}) {
+        $env->{mojito}->editer->db_name('mojito_test');
+        $env->{mojito}->linker->doc->db_name('mojito_test');
+        #$env->{mojito}->editer->collection->remove();
+    };
+    $self->app->($env);
+}
+
+1
