bb294d3f5febfdfdff6a800342ec226366163158
Mateu X. Hunter <hunter@missoula.org>
2011-03-28 18:02:34 -0600

    label commit messge

--- lib/Mojito/Template.pm
+++ lib/Mojito/Template.pm
@@ -61,7 +61,7 @@ $js_css
     <input id="mongo_id" name="mongo_id" type="hidden" form="editForm" value="" />
     <input id="wiki_language" name="wiki_language" type="hidden" form="editForm" value="" />
     <textarea id="content"  name="content" rows=32 required="required"/></textarea>
-    <input id="commit_message" name="commit_message" value="too lazy" />
+    <input id="commit_message" name="commit_message" value="commit message" onclick="this.value == 'commit message' ? this.value = '' : true"/>
     <input id="submit_save" name="submit" type="submit" value="Save" style="font-size: 66.7%;" />
     <input id="submit_view" name="submit" type="submit" value="Done" style="font-size: 66.7%;" />
 </form>
