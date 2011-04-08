8672418afcbff2f912bbf6637d7e911d861dec1f
Mateu X. Hunter <hunter@missoula.org>
2011-04-07 21:16:14 -0600

    diff arbitary distance from head route for Mojolicious

--- app/mojo.pl
+++ app/mojo.pl
@@ -8,9 +8,11 @@ use Data::Dumper::Concise;
 use JSON;
 
 # Make a shortcut the the mojito app object
-app->helper( mojito => sub {
-    return $_[0]->req->env->{mojito};
-});
+app->helper(
+    mojito => sub {
+        return $_[0]->req->env->{mojito};
+    }
+);
 
 get '/bench' => sub {
     $_[0]->render( text => $_[0]->mojito->bench );
@@ -31,24 +33,23 @@ post '/page' => sub {
 
 post '/preview' => sub {
     $_[0]->render(
-        json => $_[0]->mojito->preview_page( $_[0]->req->params->to_hash )
-    );
+        json => $_[0]->mojito->preview_page( $_[0]->req->params->to_hash ) );
 };
 
 get '/page/:id' => sub {
-    $_[0]->render( text =>
-          $_[0]->mojito->view_page( { id => $_[0]->param('id') } )
-    );
+    $_[0]->render(
+        text => $_[0]->mojito->view_page( { id => $_[0]->param('id') } ) );
 };
 
 get '/public/page/:id' => sub {
-    $_[0]->render( text =>
-          $_[0]->mojito->view_page_public( { id => $_[0]->param('id') } )
+    $_[0]->render(
+        text => $_[0]->mojito->view_page_public( { id => $_[0]->param('id') } )
     );
 };
 
 get '/page/:id/edit' => sub {
-    $_[0]->render( text => $_[0]->mojito->edit_page_form( { id => $_[0]->param('id') } ) );
+    $_[0]->render(
+        text => $_[0]->mojito->edit_page_form( { id => $_[0]->param('id') } ) );
 };
 
 post '/page/:id/edit' => sub {
@@ -57,7 +58,7 @@ post '/page/:id/edit' => sub {
     my $params = $_[0]->req->params->to_hash;
     $params->{'id'} = $_[0]->param('id');
 
-    $_[0]->redirect_to($_[0]->mojito->update_page($params));
+    $_[0]->redirect_to( $_[0]->mojito->update_page($params) );
 };
 
 get '/search/:word' => sub {
@@ -68,16 +69,32 @@ get '/search/:word' => sub {
 };
 
 get '/page/:id/delete' => sub {
-    $_[0]->redirect_to( $_[0]->mojito->delete_page({ id => $_[0]->param('id') }) );
+    $_[0]->redirect_to(
+        $_[0]->mojito->delete_page( { id => $_[0]->param('id') } ) );
 };
 
 get '/page/:id/diff' => sub {
-    $_[0]->render( text => $_[0]->mojito->view_page_diff( { id => $_[0]->param('id') } ) );
+    $_[0]->render(
+        text => $_[0]->mojito->view_page_diff( { id => $_[0]->param('id') } ) );
 };
 
+get '/page/:id/diff/:m/:n' => sub {
+
+# three params: page_id, start rev (distance from head), stop rev (distance from head)
+    $_[0]->render(
+        text => $_[0]->mojito->view_page_diff(
+            {
+                id => $_[0]->param('id'),
+                m  => $_[0]->param('m'),
+                n  => $_[0]->param('n')
+            }
+        )
+    );
+};
 
 get '/recent' => sub {
-    $_[0]->render( text => $_[0]->mojito->get_most_recent_links({want_delete_link => 1}) );
+    $_[0]->render( text =>
+          $_[0]->mojito->get_most_recent_links( { want_delete_link => 1 } ) );
 };
 
 get '/' => sub {
@@ -85,16 +102,17 @@ get '/' => sub {
 };
 
 get '/public/feed/:feed' => sub {
-    $_[0]->render( text => $_[0]->mojito->get_feed_links($_[0]->param('feed')) );
+    $_[0]
+      ->render( text => $_[0]->mojito->get_feed_links( $_[0]->param('feed') ) );
 };
 
 builder {
     enable_if { $_[0]->{PATH_INFO} !~ m/^\/(?:public|favicon.ico)/ }
-      "Auth::Digest",
-      realm => "Mojito",
-      secret => Mojito::Auth::_secret,
+    "Auth::Digest",
+      realm           => "Mojito",
+      secret          => Mojito::Auth::_secret,
       password_hashed => 1,
-      authenticator => Mojito::Auth->new->digest_authen_cb;
+      authenticator   => Mojito::Auth->new->digest_authen_cb;
     enable "+Mojito::Middleware";
     enable_if { $ENV{RELEASE_TESTING}; } "+Mojito::Middleware::TestDB";
 
