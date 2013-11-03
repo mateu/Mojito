4f292c07998165b747c244cb4ad96ed751ab5026
Mateu X Hunter <hunter@missoula.org>
2013-11-03 12:49:02 -0700

    Add new backend files for Elasticsearch

---
+++ lib/Mojito/Collection/CRUD/Elasticsearch.pm
@@ -0,0 +1,162 @@
+use strictures 1;
+package Mojito::Collection::CRUD::Elasticsearch;
+use MongoDB::OID;
+use 5.010;
+use Moo;
+use List::Util qw/first/;
+use Syntax::Keyword::Junction qw/ any /;
+use Data::Dumper::Concise;
+
+with('Mojito::Role::DB::Elasticsearch');
+
+has base_url => ( is => 'rw', );
+
+=head1 Methods
+
+=head2 create
+
+Create a list of document ids in the collection named 'collection'.
+The funny name is due to the fact we're storing document ids from
+the notes collection in individual documents of the collection collection.
+i.e. we are creating (ordered) sets of documents identified with
+a document name.
+
+The motivation for collections of docs collection is so that we can group 
+documents together form display purposes.  Form example, say I'm working 
+on a project with multiple documents.  I'd like to be able to "identify/tag" 
+them to the project so I can narrow my viewing focus to just those documents.
+
+=cut
+
+sub create {
+    my ( $self, $params ) = @_;
+
+    # We don't need to store the form submit value
+    delete $params->{collect};
+    my $page_ids = $params->{collected_page_ids};
+    
+    # NOTE: The $page_ids input can come in two different forms: 
+    # - String:   "$page_id1,$page_id2,...$page_id_n";
+    # - ArrayRef: [$page_id1,$page_id2,..., $page_id_n];
+    $params->{collected_page_ids} = [split ',', $page_ids] if (!ref($page_ids));
+    
+    # add save time as last_modified and created
+    $params->{last_modified} = $params->{created} = time();
+
+    my $collection_struct = $self->collection->{hits}{hits};
+    my @collections = map { $_->{_source} } @{$collection_struct};
+    my $collection = first { $_->{collection_name} eq $params->{collection_name} } @collections;
+    $params->{id} = 
+         $collection->{_id} 
+      || $collection->{id} 
+      || $self->generate_mongo_like_oid;
+    $self->db->index(
+        index => $self->db_name,
+        type  => $self->collection_name,
+        id    => $params->{id}, 
+        body  => $params,
+    );
+    return $params->{id};
+}
+
+=head2 read
+
+Read a collection from the database.
+
+=cut
+
+sub read {
+    my ( $self, $id ) = @_;
+    my $doc = $self->db->get_source(
+        index => $self->db_name,
+        type => $self->collection_name,
+        id => $id,
+    );
+    return $doc;
+}
+
+=head2 update
+
+Update a collection in the database.
+
+=cut
+
+sub update {
+    my ( $self, $params) = @_;
+
+    my $id = $params->{id};
+    $params->{last_modified} = time();
+    $self->db->update(
+        index => $self->db_name,
+        type  => $self->collection_name,
+        id    => $id,
+        body  =>  { doc => $params },
+    );
+}
+
+=head2 delete
+
+Delete a collection from the database.
+
+=cut
+
+sub delete {
+    my ( $self, $id ) = @_;
+    $self->collection->delete($id);
+}
+
+=head2 get_all
+
+Get all pages in the notes collection.
+Returns a MongoDB cursor one can iterate over.
+
+=cut
+
+sub get_all {
+    my $self = shift;
+    return $self->collection->{hits}{hits};
+}
+
+=head2 collection_for_page
+
+Get all the collection ids for which this page is a member of.
+
+=cut
+
+sub collections_for_page {
+    my ( $self, $page_id ) = @_;
+
+    $page_id //= '';
+    my @collection_ids = ();
+    my @collections = map { $_->{_source} } @{$self->get_all};
+    foreach my $collection (@collections) {
+        if ($page_id eq any(@{$collection->{collected_page_ids}})) {
+            push @collection_ids, $collection->{id};
+        }
+    }
+
+    return @collection_ids;
+}
+
+sub update_collection_membership {
+    my ($self, $params) = @_;
+    # Params need to include these values
+#    my $collection_ids = $params->{collection_select};
+#    my $find = {collected_page_ids => $params->{mongo_id}};
+    return;
+
+}
+=head2 BUILD
+
+Set the collection we want to work with.
+In this case it's the collection named 'collection'.
+It's a bit meta is why the funny naming.
+
+=cut
+
+sub BUILD {
+    my $self = shift;
+    $self->collection_name('collection');
+}
+
+1
---
+++ lib/Mojito/Page/CRUD/Elasticsearch.pm
@@ -0,0 +1,119 @@
+use strictures 1;
+package Mojito::Page::CRUD::Elasticsearch;
+use 5.010;
+use Moo;
+
+with('Mojito::Role::DB::Elasticsearch');
+
+has base_url => ( is => 'rw', );
+
+=head1 Name
+
+Mojito::Page::CRUD::ES - Elasticsearch CRUD
+
+=head1 Methods
+
+=head2 create
+
+Create a page in the database.
+
+=cut
+
+sub create {
+    my ( $self, $page_struct ) = @_;
+
+    # add save time as last_modified and created
+    $page_struct->{last_modified} = $page_struct->{created} = time();
+    my $oid = $self->generate_mongo_like_oid;
+    $page_struct->{id} = $oid;
+
+    # Is this sucker used ja?
+    # TODO: Seek out another when generated one is already used.
+    #die "oid: $oid is already in use" if ($self->collection && $self->collection->exists($oid));
+    # If the collection does not exist yet let's initialize it.
+    #if (!$self->collection) {
+    #    $self->db->{$self->collection_name} = {};
+    #}
+    $self->db->index(
+        index => $self->db_name,
+        type  => $self->collection_name,
+        id    => $oid, 
+        body  => $page_struct,
+    );
+    return $oid;
+}
+
+=head2 read
+
+Read a page from the database.
+
+=cut
+
+
+sub read {
+    my ($self, $id) = @_;
+    die "No id passed" if !$id;
+
+    my $doc;
+    unless (
+        eval { 
+            $doc = $self->db->get_source(
+                index => $self->db_name,
+                type => $self->collection_name,
+                id => $id,
+            ); 1;
+        }
+    ) {
+        warn "WARNING: Document with id: $id not found";
+    }
+
+    return $doc;
+}
+
+=head2 update
+
+Update a page in the database.
+
+=cut
+
+sub update {
+    my ( $self, $id, $page_struct ) = @_;
+    $page_struct->{last_modified} = time();
+    # Add in id as a key/value
+    $page_struct->{id} = $id;
+    $self->db->update(
+        index => $self->db_name,
+        type  => $self->collection_name,
+        id    => $id,
+        body  =>  { doc => $page_struct },
+    );
+}
+
+=head2 delete
+
+Delete a page from the database.
+
+=cut
+
+sub delete {
+    my ( $self, $id ) = @_;
+    $self->db->delete(
+        index => $self->db_name,
+        type  => $self->collection_name,
+        id    => $id,
+    );
+}
+
+=head2 get_all
+
+Get all pages in the notes collection.
+Returns an ArrayRef
+
+=cut
+
+sub get_all {
+    my $self = shift;
+    return $self->collection->{hits}{hits};
+}
+
+1
