25f39fe7a217437aeaec3a05d732a3f7873d2a78
Mateu X Hunter <hunter@missoula.org>
2014-09-19 14:37:45 -0600

    mongo -> elasticsearch backend transformer

--- script/mongo2elasticsearch.pl
+++ script/mongo2elasticsearch.pl
@@ -3,7 +3,8 @@ use Mojito::Model::Transform;
 use Data::Dumper::Concise;
 
 # Transform each collection: notes, collection, users
-my $t = Mojito::Model::Transform->new;
+my $transfer_type = 'elasticsearch';
+my $t = Mojito::Model::Transform->new(transfer_type => 'elasticsearch');
 my @collections = @{ $t->collections };
 foreach my $collection (@collections) {
     print "Collection: $collection\n";
